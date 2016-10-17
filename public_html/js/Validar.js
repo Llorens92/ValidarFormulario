2061ac6b402380e6cd90625d472899936f325b33
Llorens92 <tamer_1405@hotmail.com>
2016-10-17 09:18:04 +0200

    Add files via upload

--- public_html/js/Validar.js
+++ public_html/js/Validar.js
@@ -1,62 +1,98 @@
 function Validar() {
-    return ValidarNotEmpty();
+    var correcto=false;
+    if (ValidarNIF() && ValidarNomyApe(1) && ValidarNomyApe(2))
+        if (ValidarPass() && ValidarEmail()) {
+            correcto=ValidarNotEmpty();
+        } else {
+            ValidarNotEmpty();
+        }
+    return correcto;
 }
-
 function ValidarNomyApe(num) {
     var arrayCar = document.getElementsByTagName("input")[num].value.split("");
-    var correcto = true;
-    for (var i = 0; i < arrayCar.length && correcto; i++) {
-        if (!isNaN(arrayCar[i])) {
+    var letras = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "ñ", "ç", "z", "x", "c", "v", "b", "n", "m"];
+    var correcto = false;
+    var salir = false;
+    if (arrayCar.length > 0) {
+        for (var i = 0; i < arrayCar.length && !salir; i++) {
             correcto = false;
-            alert("El nombre y los apellidos solo pueden estar compuestos por letras");
+            for (var j = 0; j < letras.length && !correcto; j++) {
+                if (arrayCar[i].toLowerCase() === letras[j]) {
+                    correcto = true;
+                }
+            }
+            if (!correcto) {
+                salir = true;
+            }
         }
+        if (!correcto) {
+            alert("El nombre y los apellidos deben estar compuestos solo por letras");
+        }
+    } else {
+        correcto = true;
     }
     return correcto;
 }
 
+function ValidarPass() {
+    var arrayCar = document.getElementsByTagName("input")[5].value.split("");
+    var hayNum = false;
+    var hayLetra = false;
+    var correcto = false;
+    if (arrayCar.length < 2) {
+        document.getElementsByTagName("span")[1].setAttribute("class", "glyphicon glyphicon-remove text-danger");
+    } else {
+        for (var i = 0; i < arrayCar.length && (!hayNum || !hayLetra); i++) {
+            if (isNaN(arrayCar[i])) {
+                hayLetra = true;
+            } else {
+                hayNum = true;
+            }
+        }
+        if (hayNum && hayLetra) {
+            document.getElementsByTagName("span")[1].setAttribute("class", "glyphicon glyphicon-ok text-success");
+            correcto = true;
+        } else {
+            document.getElementsByTagName("span")[1].setAttribute("class", "glyphicon glyphicon-remove text-danger");
+        }
+    }
+    return correcto;
+
+}
 function ValidarEmail() {
     var arrayCar = document.getElementsByTagName("input")[4].value.split("");
     var correctoArro = false;
     var posArro = -1;
-    console.log("1");
+    var correcto = false;
     for (var i = 0; i < arrayCar.length && !correctoArro; i++) {
-        console.log(i);
-        console.log(arrayCar[i]);
-        console.log("  ");
         if (arrayCar[i] === "@" && i !== 0 && i < arrayCar.length - 3) {
             correctoArro = true;
             posArro = i;
         }
     }
-    console.log("2");
     var correctoPunto = false;
     for (var i = 0; i < arrayCar.length && !correctoPunto; i++) {
-        console.log(i);
-        console.log(arrayCar[i]);
-        console.log("  ");
         if (arrayCar[i] === "." && i > 2 && i < arrayCar.length - 1) {
             correctoPunto = true;
         }
     }
     var correctoNotNum = false;
-    console.log("3");
     for (var i = posArro; i < arrayCar.length; i++) {
-        console.log(i);
-        console.log(arrayCar[i]);
-        console.log("  ");
         if (isNaN(arrayCar[i])) {
             correctoNotNum = true;
         }
     }
     if (correctoArro && correctoPunto && correctoNotNum) {
-        document.getElementsByTagName("span")[0].setAttribute("class", "glyphicon glyphicon-ok text-success");  
+        document.getElementsByTagName("span")[0].setAttribute("class", "glyphicon glyphicon-ok text-success");
+        correcto = true;
     } else {
-        document.getElementsByTagName("span")[0].setAttribute("class", "glyphicon glyphicon-remove text-danger");     
+        document.getElementsByTagName("span")[0].setAttribute("class", "glyphicon glyphicon-remove text-danger");
     }
+    return correcto;
 }
 
 function ValidarNIF() {
-    return (ValidarNotEmpty() && Validar9Digitos() && ValidarNoEspacios() && Validar8PrimDig());
+    return (ValidarNotEmptyNif() && Validar9Digitos() && ValidarNoEspacios() && Validar8PrimDig());
 }
 
 function Validar8PrimDig() {
@@ -82,11 +118,22 @@ function Validar8PrimDig() {
 
 function ValidarNotEmpty() {
     var correcto = false;
-    for (var i = 0; i < document.getElementsByTagName("input").length - 1 && !correcto; i++) {
-        if (document.getElementsByTagName("input")[i].value.length === 0) {
-            alert("Debe rellenar todos los campos");
+    var form = document.forms[0];
+    for (var i = 1; i < form.length - 1 && !correcto; i++) {
+        if (form.elements[i].value.length === 0) {
+            alert("No ha rellenado el campo " + form.elements[i].name);
             correcto = true;
         }
+    }
+    return !correcto;
+}
+
+function ValidarNotEmptyNif() {
+    var correcto = false;
+    var nif = document.getElementsByTagName("input")[0].value;
+    if (nif.length === 0) {
+        alert("No ha rellenado el campo NIF");
+        correcto = true;
     }
     return !correcto;
 }
